bd8b07e64e014ef0e5b46f6892202118577c5f08
Priyanka <110306658+Priyanka3101@users.noreply.github.com>
2022-08-21 01:41:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-#streamlit.title('My Parents New Healthy Diner')
-#streamlit.header('Breakfast Menu')
-#streamlit.text('Omega 3 & Blueberry Oatmeal')
-#streamlit.text('Kale, Spinach & Rocket Smoothie')
-#streamlit.text('Hard-Boiled Free-Range Egg')
-
 streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -19,15 +13,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
-#streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
 
@@ -62,9 +51,10 @@ def get_fruit_load_list():
     return my_cur.fetchall()
   
 # Addition of button to load fruit list
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('View Our Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
@@ -74,14 +64,26 @@ def insert_row_snowflake(new_fruit):
     return "Thanks for adding the fruit " + new_fruit
  
 add_my_fruit= streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add fruit to the list'):
+if streamlit.button('Add your Favourites!'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
     
 
   
+#import pandas
     
+#streamlit.title('My Parents New Healthy Diner')
+#streamlit.header('Breakfast Menu')
+#streamlit.text('Omega 3 & Blueberry Oatmeal')
+#streamlit.text('Kale, Spinach & Rocket Smoothie')
+#streamlit.text('Hard-Boiled Free-Range Egg')
+# Let's put a pick list here so they can pick the fruit they want to include 
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+# Display the table on the page.
+#streamlit.dataframe(my_fruit_list)
+
     
 #streamlit.write('The user entered ', fruit_choice)
 #import requests
@@ -109,6 +111,7 @@ streamlit.stop()
 #my_data_rows = my_cur.fetchall()
 #streamlit.header("The fruit load list contains :")
 #streamlit.dataframe(my_data_rows)
+#if streamlit.button('Get Fruit Load List'):
 
 # Allow the end user to add a fruit to the list
 #add_my_fruit= streamlit.text_input('What fruit would you like to add?','jackfruit')
