a937a3f7a91530d34024eb9b230eb742f3e0a54a
Priyanka <110306658+Priyanka3101@users.noreply.github.com>
2022-08-20 21:51:26 +0530

    New section to display fruity vice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 # New section to display fruity vice api response
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
