3e75f271d4bad600e2fe62fbf297d2f187e285a9
Priyanka <110306658+Priyanka3101@users.noreply.github.com>
2022-08-21 00:25:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,9 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
-streamlit.text("The fruit load list contains :")
-streamlit.text(my_data_row)
+#streamlit.text("The fruit load list contains :")
+#streamlit.text(my_data_row)
+
+streamlit.header("The fruit load list contains :")
+streamlit.dataframe(my_data_row)
 
