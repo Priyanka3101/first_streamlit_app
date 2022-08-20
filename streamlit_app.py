fc22c463462e45c441ba951c78b3fb31e4143993
Priyanka <110306658+Priyanka3101@users.noreply.github.com>
2022-08-21 01:25:26 +0530

    Adding one more button

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,17 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-  
-
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
+    return "Thanks for adding the fruit " + new_fruit
+ 
+add_my_fruit= streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
     
 
   
@@ -102,6 +111,6 @@ streamlit.stop()
 #streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
-add_my_fruit= streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding the fruit ', add_my_fruit)
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
+#add_my_fruit= streamlit.text_input('What fruit would you like to add?','jackfruit')
+#streamlit.write('Thanks for adding the fruit ', add_my_fruit)
+#my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
